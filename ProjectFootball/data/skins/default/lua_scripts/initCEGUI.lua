deee592b6a517f23a444828c2a61bae2e6e27e27
dividio <dividio@19920381-7c7d-4109-9b9a-41b621a75c2c>
2007-12-17 14:41:07 +0000

    First version
    git-svn-id: https://forja.rediris.es/svn/csl2-pfootball@44 19920381-7c7d-4109-9b9a-41b621a75c2c

---
+++ ProjectFootball/data/skins/default/lua_scripts/initCEGUI.lua
@@ -0,0 +1,43 @@
+-- CEGUI initialization script
+
+-- Functions definition
+
+function cmd_back()
+    PF.CStateManager_getInstance():popState()
+end
+
+
+---------------------------
+-- CStateMainMenu functions
+---------------------------
+
+function cmd_quit()
+    PF.CStateManager_getInstance():popStack()
+end
+
+function cmd_credits()
+    state = PF.CStateCredits_getInstance()
+    PF.CStateManager_getInstance():pushState(state)
+end
+
+function cmd_play()
+    state = PF.CStateMonitor_getInstance()
+    PF.CStateManager_getInstance():pushState(state)
+end
+
+
+---------------------
+-- CCredits functions
+---------------------
+
+-- cmd_back
+
+
+---------------------
+-- CMonitor functions
+---------------------
+
+-- cmd_back
+
+
+-- End Script
