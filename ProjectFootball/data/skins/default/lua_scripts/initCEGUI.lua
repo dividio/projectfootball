69b8294d8fb591a58665e40b9b9c0664c910b34e
daviddominguez <daviddominguez@19920381-7c7d-4109-9b9a-41b621a75c2c>
2008-03-09 21:44:06 +0000

    Updated lua event handler names and added new lua event handler for CStateGame and CStateLoadGame
    git-svn-id: https://forja.rediris.es/svn/csl2-pfootball@189 19920381-7c7d-4109-9b9a-41b621a75c2c

--- ProjectFootball/data/skins/default/lua_scripts/initCEGUI.lua
+++ ProjectFootball/data/skins/default/lua_scripts/initCEGUI.lua
@@ -1,54 +1,82 @@
 -- CEGUI initialization script
 
+---------------------------
 -- Functions definition
-
+---------------------------
 function cmd_back()
     PF.CStateManager_getInstance():popState()
 end
 
 
+
 ---------------------------
 -- CStateMainMenu functions
 ---------------------------
-
-function cmd_quit()
+function cmd_Menu_Quit()
     PF.CStateManager_getInstance():popStack()
 end
 
-function cmd_credits()
+function cmd_Menu_Credits()
     local state = PF.CStateCredits_getInstance()
     PF.CStateManager_getInstance():pushState(state)
 end
 
-function cmd_play()
-    local state = PF.CStateMonitor_getInstance()
+function cmd_Menu_Load()
+    local state = PF.CStateLoadGame_getInstance()
     PF.CStateManager_getInstance():pushState(state)
 end
+
 
 
 --------------------------
 -- CStateCredits functions
 --------------------------
-
 -- cmd_back
 
 
 --------------------------
 -- CStateMonitor functions
 --------------------------
-
 -- cmd_back
 
-function cmd_startSimulation()
+function cmd_Monitor_StartSimulation()
 
 end
 
-function cmd_switchTo2DView()
+function cmd_Monitor_SwitchTo2DView()
     PF.CStateMonitor_getInstance():switchTo2DView()
 end
 
-function cmd_switchTo3DView()
+function cmd_Monitor_SwitchTo3DView()
     PF.CStateMonitor_getInstance():switchTo3DView()
+end
+
+
+
+--------------------------
+-- CStateLoadGame functions
+--------------------------
+-- cmd_back
+
+function cmd_LoadGame_New()
+    local state = PF.CStateLoadGame_getInstance()
+    state:createNewGame()
+end
+
+function cmd_LoadGame_Load()
+    local state = PF.CStateLoadGame_getInstance()
+    state:loadGame()
+end
+
+
+--------------------------
+-- CStateGame functions
+--------------------------
+-- cmd_back
+
+function cmd_Game_Play()
+    local state = PF.CStateMonitor_getInstance()
+    PF.CStateManager_getInstance():pushState(state)
 end
 
 -- End Script
